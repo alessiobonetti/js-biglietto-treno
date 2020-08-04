b2e566c29b326c96325f13eb6f1db61a54d3fce6
Alessio Bonetti <alesine87@gmail.com>
2020-08-04 15:27:16 +0200

    inserimento dati

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 // inserimento nome
 var nomeUtente = prompt("Inserisci il tuo nome");
 document.getElementById("nome_utente").innerHTML = nomeUtente;
+
 // inserimento età
+var etaUtente = prompt("Inserisci la tua età");
 
 // inserimento km
+var kmUtente = prompt("Inserisci i km del tuo viaggio");
