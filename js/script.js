45de906d04eb4a15d197e11e03028fe73c602a1b
Alessio Bonetti <alesine87@gmail.com>
2020-08-04 15:36:24 +0200

    Calcolo costo biglietto

--- js/script.js
+++ js/script.js
@@ -7,3 +7,10 @@ var etaUtente = prompt("Inserisci la tua età");
 
 // inserimento km
 var kmUtente = prompt("Inserisci i km del tuo viaggio");
+
+// costo al km
+var costoKm = 0.21
+
+// calcolo costo biglietto
+var costoBiglietto = kmUtente * costoKm;
+document.getElementById("costo_biglietto").innerHTML = costoBiglietto;
